a65494be7957f82262d884f99f2c4dedbc3a2d1c
Silvia-Pentia <80966391+Silvia-Penti@users.noreply.github.com>
2021-04-24 22:53:37 +0200

    Img source updated in JS

--- src/app.js
+++ src/app.js
@@ -22,24 +22,30 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 function displayTemperature(response) {
+  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let skyElement = document.querySelector("#sky");
+  let descriptionElement = document.querySelector("#sky");
   let dayTimeElement = document.querySelector("#day-time");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
-  skyElement.innerHTML = response.data.weather[0].desription;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   dayTimeElement.innerHTML = formatDate(response.data.dt * 1000);
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 let apiKey = "533b988d775535f72a3aabd5a735b8a7";
-let city = "Stockholm";
+let city = "Perth";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
